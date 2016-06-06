a1076ea9f64ec7c3d0b7961aaa2fe64a3c67f603
Victor Vrantchan <vrancean+github@gmail.com>
2016-06-06 19:44:38 -0400

    add release file

---
+++ cmd/scepclient/release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VERSION="0.1.0.0"
+NAME=scepclient
+OUTPUT=../../build
+
+echo "Building $NAME version $VERSION"
+
+mkdir -p ${OUTPUT}
+
+build() {
+  echo -n "=> $1-$2: "
+  GOOS=$1 GOARCH=$2 go build -o ${OUTPUT}/$NAME-$1-$2 -ldflags "-X main.version=$VERSION -X main.gitHash=`git rev-parse HEAD`" ./*.go
+  du -h ${OUTPUT}/${NAME}-$1-$2
+}
+
+build "darwin" "amd64"
+build "linux" "amd64"
