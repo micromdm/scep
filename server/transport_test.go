133594c6b6659afa2b4ef4e688ee450d2c1ed320
omorsi <78812437+omorsi@users.noreply.github.com>
2021-03-22 19:16:12 +0100

    Remove unused test functions (#161)

--- server/transport_test.go
+++ server/transport_test.go
@@ -3,13 +3,8 @@ package scepserver_test
 import (
 	"bytes"
 	"context"
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/x509"
-	"crypto/x509/pkix"
 	"encoding/base64"
-	"encoding/pem"
-	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -175,84 +170,4 @@ func loadTestFile(t *testing.T, path string) []byte {
 		t.Fatal(err)
 	}
 	return data
-}
-
-// create a new RSA private key
-func newRSAKey(bits int) (*rsa.PrivateKey, error) {
-	private, err := rsa.GenerateKey(rand.Reader, bits)
-	if err != nil {
-		return nil, err
-	}
-	return private, nil
-}
-
-// create a CSR using the same parameters as Keychain Access would produce
-func newKeychainCSR(priv *rsa.PrivateKey, email, country, cname string) ([]byte, error) {
-	subj := pkix.Name{
-		Country:    []string{country},
-		CommonName: cname,
-		ExtraNames: []pkix.AttributeTypeAndValue{{
-			Type:  []int{1, 2, 840, 113549, 1, 9, 1},
-			Value: email,
-		}},
-	}
-	template := &x509.CertificateRequest{
-		Subject: subj,
-	}
-	return x509.CreateCertificateRequest(rand.Reader, template, priv)
-}
-func loadCACredentials(t *testing.T) (*x509.Certificate, *rsa.PrivateKey) {
-	cert, err := loadCertFromFile("../scep/testdata/testca/ca.crt")
-	if err != nil {
-		t.Fatal(err)
-	}
-	key, err := loadKeyFromFile("../scep/testdata/testca/ca.key")
-	if err != nil {
-		t.Fatal(err)
-	}
-	return cert, key
-}
-
-func loadCertFromFile(path string) (*x509.Certificate, error) {
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	pemBlock, _ := pem.Decode(data)
-	if pemBlock == nil {
-		return nil, errors.New("PEM decode failed")
-	}
-	if pemBlock.Type != certificatePEMBlockType {
-		return nil, errors.New("unmatched type or headers")
-	}
-	return x509.ParseCertificate(pemBlock.Bytes)
-}
-
-// load an encrypted private key from disk
-func loadKeyFromFile(path string) (*rsa.PrivateKey, error) {
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	pemBlock, _ := pem.Decode(data)
-	if pemBlock == nil {
-		return nil, errors.New("PEM decode failed")
-	}
-	if pemBlock.Type != rsaPrivateKeyPEMBlockType {
-		return nil, errors.New("unmatched type or headers")
-	}
-
-	// testca key has a password
-	if len(pemBlock.Headers) > 0 {
-		password := []byte("")
-		b, err := x509.DecryptPEMBlock(pemBlock, password)
-		if err != nil {
-			return nil, err
-		}
-		return x509.ParsePKCS1PrivateKey(b)
-	}
-
-	return x509.ParsePKCS1PrivateKey(pemBlock.Bytes)
 }
